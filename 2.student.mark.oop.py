0feda098b726ccd4a2df6c497f904a0e3b45014e
LUciuS <96171551+DeFnd@users.noreply.github.com>
2022-04-06 11:01:02 +0700

    Update 2.student.mark.oop.py
    class Student:
        def __init__(self,name="",id="",dob=""):
            self.__name=name
            self.__id=id
            self.__dob=dob
        def getName(self):
            return self.__name
        
        def getId(self):
            return self.__id
        
        def getdob(self):
            return self.__dob
        
        def __str__(self):
            return f"Student's name is: {self.__name}, Student's id is: {self.__id}, Student's dob is: {self.__dob}"
        
        def describe(self):
            print( self.__str__())
    
        def input(self):
            self.__name=input("Student's name:")
            self.__id=input("Student's id:")
            self.__dob=input("Student's dob:")
    
    class Course:
        def __init__(self,name="",id=""):
            self.__name=name
            self.__id=id
            
        def getName(self):
            return self.__name
        
        def getId(self):
            return self.__id
        
        def __str__(self):
            return f"Course's name is: {self.__name}, Course's id is: {self.__id}"
        
        def describe(self):
            print( self.__str__())
    
        def input(self):
            self.__name=input("Course's name:")
            self.__id=input("Course's id:")
            
    class Mark:
        def __init__(self,student,course,mark=""):
            self.__student=student
            self.__course=course
            self.__mark=mark
            
        def getStudent(self):
            return self.__student
        
        def getCourse(self):
            return self.__course
        
        def getMark(self):
            return self.__mark
        
        def __str__(self):
            return f"Student name: {self.__name} - Course's id is: {self.__id}- has mark:{self.__mark}"
        
        def describe(self):
            print( self.__str__())
    
        def input(self):
            self.__mark=input("Enter mark of student name: {self.__student.getName},course {self.__course.getName}:")
            
    
    class Management:
        def list(self):
            pass
        
        def input(self):
            pass
    
    class StudentManagement(Management):
        def __init__(self):
            self.__stds=[]
            self.__cours=[]
            self.__marks=[]
            
        def getStudent(self,idx):
            return self.__stds[idx]
        
        def getCourse(self,idx):
            return self.__cours[idx]
        
        def getMark(self,idx):
            return self.__marks[idx]
        
        def inputStudent(self):
            s=Student()
            s.input()
            self.__stds+=[s]
        
        def inputCourse(self):
            c=Course()
            c.input()
            self.__cours+=[c]
        
        def inputMark(self):
            for i in range(len(self.__stds)):
                m0=[]
                for j in range (len(self.__cours)):
                    c=int(input(f"Enter mark of student name {self.__stds[i].getName()} - course {self.__cours[j].getName()}"))
                    m0+=[c]
                self.__marks+=[m0]
        
            
        def listMark(self):
            for i in range(len(self.__stds)):
                for j in range (len(self.__cours)):
                    print (f"Mark of student name {self.__stds[i].getName()} - course {self.__cours[j].getName()} is :{self.__marks[i][j]}")
        def list(self):
            for i in self.__stds:
                i.describe()
    
    
    NumberOfStd=int(input("\nHow many students are there?"))
    studentmanagement=StudentManagement()
    for i in range(NumberOfStd):
        studentmanagement.inputStudent()
    NumberOfCour=int(input("\nHow many courses are there?"))
    for j in range(NumberOfCour):
        studentmanagement.inputCourse()
    studentmanagement.inputMark()
    studentmanagement.listMark()

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,210 +1,130 @@
-# ------------------------------ Student -------------------------------------
 class Student:
+    def __init__(self,name="",id="",dob=""):
+        self.__name=name
+        self.__id=id
+        self.__dob=dob
+    def getName(self):
+        return self.__name
+    
+    def getId(self):
+        return self.__id
+    
+    def getdob(self):
+        return self.__dob
+    
+    def __str__(self):
+        return f"Student's name is: {self.__name}, Student's id is: {self.__id}, Student's dob is: {self.__dob}"
+    
+    def describe(self):
+        print( self.__str__())
 
-    def __init__(self, name, std_id, dob):
-        self._std_Name = name
-        self._std_ID = std_id
-        self._std_DoB = dob
+    def input(self):
+        self.__name=input("Student's name:")
+        self.__id=input("Student's id:")
+        self.__dob=input("Student's dob:")
 
-    def set_std_name(self, name):
-        self._std_Name = name
-
-    def set_std_id(self, std_id):
-        self._std_ID = std_id
-
-    def set_std_dob(self, dob):
-        self._std_DoB = dob
-
-    def get_st_name(self):
-        return self._std_Name
-
-    def get_st_id(self):
-        return self._std_ID
-
-    def get_st_dob(self):
-        return self._std_DoB
-
-# ------------------------------ Course -------------------------------------
 class Course:
-    def __init__(self, name, c_id):
-        self._name = name
-        self._id = c_id
+    def __init__(self,name="",id=""):
+        self.__name=name
+        self.__id=id
+        
+    def getName(self):
+        return self.__name
+    
+    def getId(self):
+        return self.__id
+    
+    def __str__(self):
+        return f"Course's name is: {self.__name}, Course's id is: {self.__id}"
+    
+    def describe(self):
+        print( self.__str__())
 
-    def set_c_name(self, name):
-        self._name = name
+    def input(self):
+        self.__name=input("Course's name:")
+        self.__id=input("Course's id:")
+        
+class Mark:
+    def __init__(self,student,course,mark=""):
+        self.__student=student
+        self.__course=course
+        self.__mark=mark
+        
+    def getStudent(self):
+        return self.__student
+    
+    def getCourse(self):
+        return self.__course
+    
+    def getMark(self):
+        return self.__mark
+    
+    def __str__(self):
+        return f"Student name: {self.__name} - Course's id is: {self.__id}- has mark:{self.__mark}"
+    
+    def describe(self):
+        print( self.__str__())
 
-    def set_c_id(self, c_id):
-        self._id = c_id
+    def input(self):
+        self.__mark=input("Enter mark of student name: {self.__student.getName},course {self.__course.getName}:")
+        
 
-    def get_name(self):
-        return self._name
-
-    def get_id(self):
-        return self._id
-
-# ------------------------------ Student Management -------------------------------------
-class StudentManagement:
-    _st_Name = ""
-    _crs_Inf = {}
-
-    def __init__(self):
+class Management:
+    def list(self):
+        pass
+    
+    def input(self):
         pass
 
-    def set_inf(self, name, course_inf):
-        self._st_Name = name
-        self._crs_Inf = course_inf
-
-    def set_st_name(self, student_name):
-        self._st_Name = student_name
-
-    def get_st_name(self):
-        return self._st_Name
-
-    def set_crs_inf(self, dic_inf):
-        self._crs_Inf = dic_inf
-
-    def get_crs_inf(self):
-        return self._crs_Inf
-
-
-# ------------------------------ Database -------------------------------------
-std_list = [Student("data", "data", "data")]
-course_list = [Course("data", "data")]
-stm_list = [StudentManagement()]
-
-std_list.clear()
-course_list.clear()
-stm_list.clear()
-
-count_smm = 0
-
-
-# ------------------------------ Method -------------------------------------
-
-
-def input_domain(value, min_value, max_value):
-    while value < min_value or value > max_value:
-        value = int(input("Your input is invalid! Enter again: "))
-    return value
-
-
-def exit_menu():
-    global option
-    print("---------------------End of this choice---------------------")
-    option = int(input("Make other choice, 0 to exit: "))
-    option = input_domain(option, 0, 6)
-    if option == 0:
-        print("You choose option exit!!!")
+class StudentManagement(Management):
+    def __init__(self):
+        self.__stds=[]
+        self.__cours=[]
+        self.__marks=[]
+        
+    def getStudent(self,idx):
+        return self.__stds[idx]
+    
+    def getCourse(self,idx):
+        return self.__cours[idx]
+    
+    def getMark(self,idx):
+        return self.__marks[idx]
+    
+    def inputStudent(self):
+        s=Student()
+        s.input()
+        self.__stds+=[s]
+    
+    def inputCourse(self):
+        c=Course()
+        c.input()
+        self.__cours+=[c]
+    
+    def inputMark(self):
+        for i in range(len(self.__stds)):
+            m0=[]
+            for j in range (len(self.__cours)):
+                c=int(input(f"Enter mark of student name {self.__stds[i].getName()} - course {self.__cours[j].getName()}"))
+                m0+=[c]
+            self.__marks+=[m0]
+    
+        
+    def listMark(self):
+        for i in range(len(self.__stds)):
+            for j in range (len(self.__cours)):
+                print (f"Mark of student name {self.__stds[i].getName()} - course {self.__cours[j].getName()} is :{self.__marks[i][j]}")
+    def list(self):
+        for i in self.__stds:
+            i.describe()
 
 
-def option1():
-    global i
-    num_of_std = int(input("Input number of student in the class: "))
-    for i in range(0, num_of_std):
-        name = str(input("Student's Name: "))
-        student_id = str(input("Student's ID: "))
-        dob = str(input("Student's Date of Birth: "))
-        std_list.append(Student(name, student_id, dob))
-
-
-def option2():
-    global i
-    num_of_crs = int(input("Number of courses: "))
-    for i in range(0, num_of_crs):
-        c_name = str(input("Course's name: "))
-        c_id = str(input("Course's ID: "))
-        course_list.append(Course(c_name, c_id))
-
-
-def option3():
-    global option, i, count_smm
-    name = str(input("Name of student you want to manage: "))
-    cour_inf_lit = {}
-    if CheckStudentList(name):
-        stm_list.append(StudentManagement())
-        stm_list[count_smm].set_st_name(name)
-        num_of_course = int(input("Number of courses this student studied: "))
-        for i in range(0, num_of_course):
-            c_name = input("Name of course: ")
-            if CheckCourseList(c_name):
-                mark = int(input("Mark of this course: "))
-                cour_inf_lit[c_name] = mark
-                stm_list[count_smm].set_crs_inf(cour_inf_lit)
-            else:
-                print("Input course did not import.")
-        count_smm += 1
-    else:
-        option = int(input("Input wrong ! Press 3 to do again, 0 to exit: !"))
-
-
-def CheckStudentList(name):
-    global i
-
-    check = False
-    for i in std_list:
-        if name == i.get_st_name():
-            check = True
-    return check
-
-
-def CheckCourseList(c_name):
-    global i
-    for i in course_list:
-        if i.get_name() == c_name:
-            return True
-
-
-# ------------------------------ Menu -------------------------------------
-print("Program started !!!")
-
-def menu_option():
-    print("""
-============================================================================
-Here are the menu option, please choose!!!!
-    0. Exit
-    1. Input information of students
-    2. Input information of courses
-    3. Input mark for students by courses
-    4. Output information of students
-    5. Output information of courses
-    6. Output information for students course management """)  
-menu_option()
-option = int(input("Enter your choice: "))
-
-while option != 0:
-    if option == 1:
-        option1()
-        exit_menu()
-
-    elif option == 2:
-        option2()
-        exit_menu()
-
-    elif option == 3:
-        option3()
-        exit_menu()
-
-    elif option == 4:
-        print("All information of student in class: ")
-        for i in std_list:
-            print("Student's name = " + i.get_st_name(), end=". Student's ID = ")
-            print(i.get_st_id(), end=". Date of Birth = " + i.get_st_dob())
-            print()
-        exit_menu()
-
-    elif option == 5:
-        print("All information of course: ")
-        for i in course_list:
-            print("Course's name = " + i.get_name(), end=". ID = ")
-            print(i.get_id())
-        exit_menu()
-
-    elif option == 6:
-        for i in stm_list:
-            print("This is mark information of " + i.get_st_name() + ": ", end="")
-            print(i.get_crs_inf())
-        exit_menu()
-
-    else:
-        print("You choose option exit!!!")
-        break
+NumberOfStd=int(input("\nHow many students are there?"))
+studentmanagement=StudentManagement()
+for i in range(NumberOfStd):
+    studentmanagement.inputStudent()
+NumberOfCour=int(input("\nHow many courses are there?"))
+for j in range(NumberOfCour):
+    studentmanagement.inputCourse()
+    studentmanagement.inputMark()
+    studentmanagement.listMark()
